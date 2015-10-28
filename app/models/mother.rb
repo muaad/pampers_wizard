1afe54bc31ae509965e9cd91614942c729f70c3e
Muaad <muadh24@gmail.com>
2015-10-28 16:37:16 +0300

    Added some Chat related methods to Mother

--- app/models/mother.rb
+++ app/models/mother.rb
@@ -1,3 +1,36 @@
 class Mother < ActiveRecord::Base
 	belongs_to :account
+	has_many :chats
+	has_many :friends, :through => :chats
+
+	def chats
+		Chat.where("mother_id = ? OR friend_id = ?", id, id)
+	end
+
+	def chats_with mother
+		chats.where("mother_id = ? OR friend_id = ?", mother.id, mother.id)
+	end
+
+	def last_chat
+		chats.order("updated_at desc").first
+	end
+
+	def active_chats
+		chats.active
+	end
+
+	def buddies
+		buds = []
+		chats.each do |chat|
+			recipient = chat.recipient(self)
+			if !recipient.nil?
+				buds << "@#{recipient.username}"
+			end
+		end
+		buds
+	end
+
+	def self.username_exists? username
+		!Mother.where("username like '#{username}'").empty?
+	end
 end
