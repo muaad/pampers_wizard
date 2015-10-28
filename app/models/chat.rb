a11577e87c6e13cf46f52cf2133ecc0dd0b596aa
Muaad <muadh24@gmail.com>
2015-10-28 15:56:56 +0300

    Added method to get recipient of chat to Chat, active scope

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,16 @@
 class Chat < ActiveRecord::Base
   belongs_to :mother
-  belongs_to :friend
+  belongs_to :friend, :class_name => "Mother"
+
+  scope :active, -> { where(active: true) }
+
+  def recipient sender
+  	receiver = nil
+  	if mother == sender
+  		receiver = friend
+  	else
+  		receiver = mother
+  	end
+  	receiver
+  end
 end
